c10da8e2ad44586316987cd7fa92e57460ca56f5
RobinvargheseO4 <84446729+RobinvargheseO4@users.noreply.github.com>
2023-04-06 10:27:06 +0530

    Create about us.php
    about us

---
+++ about us.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My PHP Page</title>
+</head>
+<body>
+
+    <h1>Welcome to my PHP Page</h1>
+
+    <?php
+        // PHP code goes here
+        echo "Hello, World!";
+    ?>
+
+</body>
+</html>
